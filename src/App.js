a21e0cb8b5489d46627381264dcc548a51f8b8f1
RAO YANG <lightrao@hotmail.com>
2022-12-04 11:15:05 +0800

    Working with "State"

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // ./App.js
-import React from "react";
+import React, { useState } from "react";
 
 // component start with an uppercase character
 import GoalList from "./components/GoalList";
@@ -9,16 +9,15 @@ import NewGoal from "./components/NewGoal";
 import "./App.css";
 
 const App = () => {
-  const courseGoals = [
+  const [courseGoals, setCourseGoals] = useState([
     { id: "cg1", text: "Learn Javascript" },
     { id: "cg2", text: "Learn React" },
     { id: "cg3", text: "Learn Redux" },
     { id: "cg4", text: "Help others in course Q&A" },
-  ];
+  ]);
 
   const addNewGoalHandler = (newGoal) => {
-    courseGoals.push(newGoal);
-    console.log(courseGoals);
+    setCourseGoals(courseGoals.concat(newGoal));
   };
 
   return (
