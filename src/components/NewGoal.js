42c4c29cc46833d5883718552d0a47280908f8cd
RAO YANG <lightrao@hotmail.com>
2022-12-04 13:41:25 +0800

    use nomal var to fech user in put

--- src/components/NewGoal.js
+++ src/components/NewGoal.js
@@ -3,18 +3,26 @@ import React from "react";
 import "./NewGoal.css";
 
 const NewGoal = (props) => {
+  let enteredText = ""; // variable will not lead to UI rerender
+
   const addGoalHandler = (event) => {
     event.preventDefault();
     const newGoal = {
       id: Math.random().toString(),
-      text: "A new goal!",
+      text: enteredText,
     };
+
     props.onAddGoal(newGoal);
+    enteredText = ""; // this is not reflected on our input
+  };
+
+  const textChangeHandler = (event) => {
+    enteredText = event.target.value;
   };
 
   return (
     <form className="new-goal" onSubmit={addGoalHandler}>
-      <input type="text" />
+      <input type="text" value={enteredText} onChange={textChangeHandler} />
       <button type="submit">Add Goal</button>
     </form>
   );
